d98b62db49e59761513f5c95f750e2e0af3df014
Petar Šegina <psegina@ymail.com>
2016-10-15 17:16:16 +0200

    Listen and handle events

--- src/github.com/mrPjer/mocker/mocker.go
+++ src/github.com/mrPjer/mocker/mocker.go
@@ -25,4 +25,18 @@ func main() {
 		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
 	}
 
+	events, _ := cli.Events(context.Background(), types.EventsOptions{})
+
+	for event := range events {
+		var name = event.Actor.Attributes["name"]
+		var image = event.From
+		var id = event.ID
+		var action = event.Action
+		handleContainerEvent(name, image, id, action)
+	}
+
+}
+
+func handleContainerEvent(name, image, id, action string) {
+	fmt.Printf("Name: %s (%s)\tID: %s\tAction: %s\n", name, image, id, action)
 }
