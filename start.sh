c8b0ce6b3cf98aa2dcb76456b398fdb2ec9e3313
Petar Šegina <psegina@ymail.com>
2016-11-11 02:24:30 +0100

    Manually specify Docker API version
    Otherwise the client and the server may be out of sync, causing the
    client to stop working.

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+export DOCKER_API_VERSION=1.24;
 nohup /app/listener/bin/mocker 2>&1> /var/log/mocker.log  &
 nohup nodejs /app/twillio/index.js 2>&1> /var/log/twilio.log &
 nohup nodejs /app/twillio/notifier.js 2>&1> /var/log/twilio-not.log &
